52d63980984321832b0303747a0c868316836ad4
Note45 <antonion.2048@gmail.com>
2022-07-08 09:53:00 -0300

    add resolve user

--- api/user/resolvers/userResolvers.js
+++ api/user/resolvers/userResolvers.js
@@ -2,6 +2,8 @@ const userResolvers = {
   Query: {
     users: (root, args, { dataSources }, info) => dataSources
       .usersAPI.getUsers(),
+    user: (root, { id }, { dataSources }, info) => dataSources
+      .usersAPI.getUserById(id)
   }
 }
 
